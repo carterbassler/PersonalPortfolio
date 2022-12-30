f7202dbab7196386346cbfe6cce14a37dfb24c69
Carter Bassler <carter@Carters-MacBook-Pro.local>
2022-12-30 14:17:04 -0500

    Fix Heaven

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -36,7 +36,7 @@ function ExperienceCard({experience}: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0}}
         viewport={{once: true }}
-        className='w-28 h-28 md:w-32 md:h-32 rounded-full md:rounded-full xl:w-[130px]
+        className='w-24 h-24 md:w-32 md:h-32 rounded-full md:rounded-full xl:w-[130px]
         xl:h-[130px] object-cover object-center'
         src={experience.image}
         alt=""
@@ -45,7 +45,7 @@ function ExperienceCard({experience}: Props) {
         <div className='px-0 md:px-10'>
             <h4 className='text-2xl md:text-2xl font-light'>{experience.company}</h4>
             <p className='font-bold text-lg md:text-xl mt-1'>{experience.role}</p>
-            <p className='uppercase py-4 text-base md:text-lg text-gray-300'>{experience.duration}</p>
+            <p className='py-2 md:py-4 text-base text-gray-300'>{experience.duration}</p>
 
             <ul className='list-disc space-y-3 ml-5 text-xs md:text-sm xl:text-base'>
                 {experience.bulletPoints.map(bulletPoint =>(
